ee30e024f828bcb05ecd753b71bd871dbbbec530
Zane Reda <redazanelc@gmail.com>
2026-06-19 13:48:04 -0500

    Stable v 0.2

--- home/user/programs/ssh.nix
+++ home/user/programs/ssh.nix
@@ -1,58 +1,20 @@
 { config, pkgs, inputs, ... }:
 
 {
-  programs.git = {
+programs.ssh = {
     enable = true;
     
-    # 5. Enable Git Large File Storage (LFS) globally if needed
-    # Note: lfs.enable remains a top-level module option, not part of settings.
-    lfs.enable = true;
-
-    # 3. Global Gitignore rules (Uncommented just in case you want them, 
-    # but still a top-level array option in Home Manager)
-    # ignores = [ 
-    #   "*~" 
-    #   "*.swp" 
-    #   ".DS_Store" 
-    # ];
-
-    # The new unified settings block replacing extraConfig, userName, userEmail, and aliases
-    settings = {
-      # 1. Global User Identity
-      user = {
-        name = "Zane Reda";
-        email = "redazanelc@gmail.com";
-      };
-
-      # 2. Global Aliases
-      alias = {
-        st = "status";
-        co = "checkout";
-        cm = "commit";
-        rb = "rebuild";
-      };
-
-      # 4. Advanced/Global INI Settings
-      init = {
-        defaultBranch = "main";
-      };
-      
-      push = {
-        autoSetupRemote = true;
-      };
-
-      safe = {
-        directory = [
-          "/home/user/nixos-config"
-        ];
-      };
-
-      core = {
-        editor = "micro"; 
+    # Automatically add GitHub to known hosts so Nix doesn't ask 
+    # "Are you sure you want to continue connecting?" on a new machine.
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        identityFile = "~/.ssh/id_ed25519"; # Tells Git exactly where to look for this machine's key
       };
     };
   };
 }
+
 
 /*
 =============================================================================
